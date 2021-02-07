7a96e6313a43e995b67bb2c91497b5c3657f3c1d
shahibur.shanto <shahibur407@gmail.com>
2021-02-07 22:45:27 +0600

    submit solution

---
+++ script.js
@@ -0,0 +1,54 @@
+document.getElementById("btn-submit").addEventListener("click", displayFoods)
+
+function displayFoods() {
+    document.getElementById("detail-foods").innerHTML = "";
+    document.getElementById("food-category").innerHTML = "";
+    const foodDiv = document.getElementById("food-category");
+    const input = document.getElementById("search-item").value;
+
+    fetch(`https://themealdb.com/api/json/v1/1/search.php?f=${input[0]}`)
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < data.meals.length; i++) {
+                let mealName = data.meals[i].strMeal;
+                let mealImg = data.meals[i].strMealThumb;
+                const displayDiv = document.createElement("div");
+
+                displayDiv.className = "display-div";
+                displayDiv.innerHTML = `
+                <img src='${mealImg}'>
+                <h5 class="meal-name">${mealName}</h5>
+                `;
+                foodDiv.appendChild(displayDiv);
+
+                displayDiv.addEventListener('click', function () {
+                    document.getElementById("detail-foods").innerHTML = "";
+                    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+                        .then(res => res.json())
+                        .then(data => {
+
+                            const displayDetails = document.getElementById("detail-foods");
+                            displayDetails.innerHTML = `
+                    <img src="${mealImg}">
+                    <h5 class="meal-name">${mealName}</h5>
+                    <p>Ingredients</p>
+                    <ul>
+                    <li>${data.meals[0].strIngredient1}</li>
+                    <li>${data.meals[0].strIngredient2}</li>
+                    <li>${data.meals[0].strIngredient3}</li>
+                    <li>${data.meals[0].strIngredient4}</li>
+                    <li>${data.meals[0].strIngredient5}</li>
+                    <li>${data.meals[0].strIngredient6}</li>
+                    <li>${data.meals[0].strIngredient7}</li>
+                    <li>${data.meals[0].strIngredient8}</li>
+                    <li>${data.meals[0].strIngredient9}</li>
+                    <li>${data.meals[0].strIngredient10}</li>
+                    </ul>      
+                   `
+
+                        })
+                });
+
+            }
+        })
+}
