d2085dc8b9b30c52876f75573d5ded6aaa9e45ba
shahibur.shanto <shahibur407@gmail.com>
2021-02-08 21:44:55 +0600

    catch error

--- script.js
+++ script.js
@@ -23,10 +23,11 @@ displayDetails = (data, img, name) => {
 function displayFoods() {
     document.getElementById("detail-foods").innerHTML = "";
     document.getElementById("food-category").innerHTML = "";
+    document.getElementById("error").innerHTML="";
     const foodDiv = document.getElementById("food-category");
     const input = document.getElementById("search-item").value;
-
-    fetch(`https://themealdb.com/api/json/v1/1/search.php?f=${input[0]}`)
+    const url =`https://themealdb.com/api/json/v1/1/search.php?f=${input[0]}` 
+    fetch(url)
         .then(res => res.json())
         .then(data => {
             for (let i = 0; i < data.meals.length; i++) {
@@ -48,5 +49,10 @@ function displayFoods() {
                         .then(data => displayDetails(data, mealImg, mealName))
                 });
             }
+        })
+        .catch(error => {
+            document.getElementById("error").innerHTML =   `
+            <h3>Your Food not found. please try another food.</h3>
+            `
         })
 }
