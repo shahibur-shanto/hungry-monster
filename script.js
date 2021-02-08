38f6144a83de40af19306e5a1a81515a440c118e
shahibur.shanto <shahibur407@gmail.com>
2021-02-08 23:37:08 +0600

    deleting all comments

--- script.js
+++ script.js
@@ -23,14 +23,12 @@ function displayFoods() {
                 let mealName = data.meals[i].strMeal;
                 let mealImg = data.meals[i].strMealThumb;
                 const displayDiv = document.createElement("div");
-
                 displayDiv.className = "display-div";
                 displayDiv.innerHTML = `
                 <img src='${mealImg}'>
                 <h5 class="meal-name">${mealName}</h5>
                 `;
                 foodDiv.appendChild(displayDiv);
-
                 displayDiv.addEventListener('click', () => {
                     document.getElementById("detail-foods").innerHTML = "";
                     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
@@ -42,15 +40,13 @@ function displayFoods() {
         .catch(error => {
             document.getElementById("error").innerHTML = `
             <h3>Your Food not found. please try another food.</h3>
-            `
+        `
         })
 }
 
 function ingraList(data) {
-    //const ingredientList = Object.keys(data.meals[0]);
     const ingredientList = Object.keys(data);
     let ul = document.createElement("ul");
-
     let list = [];
     for (let i = 9; i <= 28; i++) {
         list.push(ingredientList[i])
@@ -58,7 +54,6 @@ function ingraList(data) {
     list.forEach((key, index) => {
         if (data[key] == null || data[key] == "") {
             ul.innerHTML = ul.innerHTML;
-
         }
         else {
             ul.innerHTML += `<li>${data[key]}</li>`;
